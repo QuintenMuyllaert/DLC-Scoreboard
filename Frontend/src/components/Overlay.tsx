5e293a3c6a8d53de3edbe565a8b3d4e766dfbc70
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-01 10:15:13 +0200

    ✨ Made overlay component

---
+++ Frontend/src/components/Overlay.tsx
@@ -0,0 +1,14 @@
+export const Overlay = ({ visible, setVisible = () => {}, children }: { visible: boolean; setVisible?: (event?: any) => any; children?: any }) => {
+	if (visible) {
+		return (
+			<div className="c-overlay">
+				<div className="c-overlay-x" onClick={() => setVisible(false)}></div>
+				<div className="c-overlay-content"> {children}</div>
+			</div>
+		);
+	} else {
+		return <></>;
+	}
+};
+
+export default Overlay;
