c8aa51a0ce43fe40c2b20d68c3224419460abb63
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-01 10:40:10 +0200

    🎨 Clockdata interface

--- Frontend/src/utils/Interfaces.ts
+++ Frontend/src/utils/Interfaces.ts
@@ -1,3 +1,11 @@
 export interface LooseObject {
 	[key: string]: any;
 }
+
+export interface clockData {
+	realTime: boolean;
+	paused: boolean;
+	startTime: number;
+	startPauseTime: number;
+	pauseTime: number;
+}
