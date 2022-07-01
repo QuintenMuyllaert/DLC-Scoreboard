86dba71e952df154f1933147a300a747d8c21c71
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-01 15:10:34 +0200

    ♻️ refactor onClick

--- Frontend/src/components/Flag.tsx
+++ Frontend/src/components/Flag.tsx
@@ -1,6 +1,6 @@
-export const Flag = ({ top, bottom, handleClickPopup }: { top: string; bottom: string; handleClickPopup?: (event?: any) => any }) => {
+export const Flag = ({ top, bottom, onClick = () => {} }: { top: string; bottom: string; onClick?: (event?: any) => any }) => {
 	return (
-		<button className="c-flag" onClick={handleClickPopup ? handleClickPopup : () => {}}>
+		<button className="c-flag" onClick={onClick}>
 			<div className="top" style={{ backgroundColor: top }}></div>
 			<div className="bottom" style={{ backgroundColor: bottom }}></div>
 		</button>
