082cd65bd082348d9c57bc6720393a417069a031
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-04 10:10:02 +0200

    ⚰️ Removed refetch

--- Frontend/src/pages/Refetch.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { getQuery } from "../utils/Utils";
-
-export const Root = () => {
-	const { page } = getQuery();
-	return <Navigate to={`/${page || ""}`} />;
-};
-
-export default Root;
