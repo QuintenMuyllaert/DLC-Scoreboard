8ad94e3195dab8dc1645b17ecb8af72d4c7a7669
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-06 16:19:41 +0200

    ⚡️ Generate has SVG & HTML support

--- Backend/src/generate.ts
+++ Backend/src/generate.ts
@@ -1,3 +1,5 @@
+import { HMP } from "../../Interfaces/Interfaces";
+
 const width = 1920; //588
 const height = 1080; //336
 
@@ -5,7 +7,7 @@ const thickness = 0.005 * width;
 const fontSize = 0.06875 * width; //132;
 const fontSizeMessage = 0.0864 * width; //166;
 
-let dastuff = `<rect fill="black" height="${height}" id="background" stroke="none" width="${width}"/>
+const svgElement = `<rect fill="black" height="${height}" id="background" stroke="none" width="${width}"/>
 
 <g transform="translate(${0.145 * width} ${0.023 * height})">
 	<rect x="0" y="0" width="${0.15 * width}" height="${0.5 * 0.15 * height}" id="hb" stroke="none" />
@@ -39,125 +41,155 @@ let dastuff = `<rect fill="black" height="${height}" id="background" stroke="non
 <rect fill="black" width="${width}" height="${height}" id="sponsorbackground" stroke="none"/>
 <image id="sponsorimg" x="0" y="0" width="${width}" height="${height}" xlink:href=""  />`;
 
-let daScript = `
-		var len = 16;
+const scriptSVG = `
+	var len = 16;
 
-		socket.on("data",function (element,thing,type,value){
-			//$("#wauw").attr('style',dat[0]+": "+dat[1]); // update data
-			if(value){
-				$(element)[thing](type,value);
-			}else{
-				$(element)[thing](type); 
-			}
+	socket.on("data",function (element,thing,type,value){
+		//$("#wauw").attr('style',dat[0]+": "+dat[1]); // update data
+		if(value){
+			$(element)[thing](type,value);
+		}else{
+			$(element)[thing](type); 
+		}
+		if(element == "#message" && thing == "text"){
+			len = type.length;
+		}
+	});
 
-			if(element == "#message" && thing == "text"){
-				len = type.length;
-			}
-		});
-
-		var clockData = {
-			realTime: false,
-			paused: false,
-			startTime: Date.now(),
-			startPauseTime: 0,
-			pauseTime: 0,
-		};
+	var clockData = {
+		realTime: false,
+		paused: false,
+		startTime: Date.now(),
+		startPauseTime: 0,
+		pauseTime: 0,
+	};
  
-		function to2digits(num){
-			if(num < 10){
-				return "0" + num;
-			}
-			return num;
+	function to2digits(num){
+		if(num < 10){
+			return "0" + num;
+		}
+		return num;
+	}
+
+	function clockify(time) {
+		var totalSeconds = time / 1000;
+		var minutes = Math.floor(totalSeconds / 60);
+		var seconds = Math.floor(totalSeconds % 60);
+		return to2digits(minutes) + ":" + to2digits(seconds);
+	}
+
+	socket.on("clockData",function(data){
+		clockData = data;
+	});
+
+	$("#sponsorbackground").attr("style","opacity: 0");
+	var sponsors = [];
+	var sponsorIndex = 0;
+	socket.on("sponsors",function(data){
+		if(!data){
+			socket.emit("echo",data,"no data");
+			return;
+		}
+		sponsors = data;
+	});
+
+	setInterval(function(){
+		sponsorIndex++;
+		var data = sponsors[sponsorIndex % sponsors.length];
+		$("#sponsorimg").attr("xlink:href",data);
+		$("#sponsorimgsmall").attr("xlink:href",data);
+	},5000);
+
+	socket.on("fullscreen",function(data){
+		if(data){
+			$("#sponsorbackground").attr("style","opacity: 1");
+			$("#sponsorimg").attr("style","opacity: 1");
+			$("#sponsorimgsmall").attr("style","opacity: 0");
+		}else{
+			$("#sponsorbackground").attr("style","opacity: 0");
+			$("#sponsorimg").attr("style","opacity: 0");
+			$("#sponsorimgsmall").attr("style","opacity: 1");
+		}
+	});
+
+	setInterval(function(){
+		var display = "00:00";
+		var now = Date.now();
+
+		if (clockData.paused) {
+			var delta = clockData.startPauseTime - clockData.startTime - clockData.pauseTime;
+			display = clockify(delta);
+		} else {
+			var delta = now - clockData.startTime - clockData.pauseTime;
+			display = clockify(delta);
 		}
 
-		function clockify(time) {
-			var totalSeconds = time / 1000;
-			var minutes = Math.floor(totalSeconds / 60);
-			var seconds = Math.floor(totalSeconds % 60);
-			return to2digits(minutes) + ":" + to2digits(seconds);
+		if (clockData.realTime) {
+			//get clock as 24hour 00:00
+			var seconds = 60 * new Date(now).getHours() + new Date(now).getMinutes();
+
+			display = clockify(1000 * seconds);
 		}
 
-		socket.on("clockData",function(data){
-			clockData = data;
-		});
+		$("#timer").text(display);
+	},50);
 
-		$("#sponsorbackground").attr("style","opacity: 0");
-		var sponsors = [];
-		var sponsorIndex = 0;
-		socket.on("sponsors",function(data){
-			if(!data){
-				socket.emit("echo",data,"no data");
-				return;
-			}
-			sponsors = data;
-		});
+	var pos = ${width};
+	setInterval(function(){
+		$("#message").attr("x",pos);
+		pos = pos - 0.0034 * ${width};
+		if(pos < -len * ${fontSizeMessage}){
+			pos = ${width};
+		}
+	},16);
 
-		setInterval(function(){
-			sponsorIndex++;
-			var data = sponsors[sponsorIndex % sponsors.length];
-			socket.emit("echo",sponsorIndex);
-			socket.emit("echo",data);
+	$("svg").attr("viewBox","0 0 ${width} ${height}");
+`;
 
-			$("#sponsorimg").attr("xlink:href",data);
-			$("#sponsorimgsmall").attr("xlink:href",data);
-		},5000);
+const scriptHTML = `
+	var root = [
+		{
+			args: {
+				height: ${height},
+				id: "YBkasrcXK8",
+				left: 0,
+				repeatDur: "indefinite",
+				src: "https://dlcscoreboard.computernetwork.be/scoreboard?serial=" + deviceInfo.serialNumber,
+				top: 0,
+				width: ${width},
+			},
+			changeNumber: "1.67",
+			ctor: "iframe",
+		},
+	];	
+	
+	var layers = [
 
-		socket.on("fullscreen",function(data){
-			if(data){
-				$("#sponsorbackground").attr("style","opacity: 1");
-				$("#sponsorimg").attr("style","opacity: 1");
-				$("#sponsorimgsmall").attr("style","opacity: 0");
-			}else{
-				$("#sponsorbackground").attr("style","opacity: 0");
-				$("#sponsorimg").attr("style","opacity: 0");
-				$("#sponsorimgsmall").attr("style","opacity: 1");
-			}
-		});
+	];
 
-		setInterval(function(){
-			var display = "00:00";
-			var now = Date.now();
+	$('#root').add( $.uncan( root ) );	
+	$('#layers').add( $.uncan( layers ) );	
+`;
 
-			if (clockData.paused) {
-				var  delta = clockData.startPauseTime - clockData.startTime - clockData.pauseTime;
-				display = clockify(delta);
-			} else {
-				var  delta = now - clockData.startTime - clockData.pauseTime;
-				display = clockify(delta);
-			}
+const HTMLSupport = ["inanis", "windows", "fukiran"];
+const noHTMLSupport = ["bonsai", "sakura", "ikebana"];
 
-			if (clockData.realTime) {
-				//get clock as 24hour 00:00
-				var seconds = 60 * new Date(now).getHours() + new Date(now).getMinutes();
+export const Generate = (hmp: HMP) => {
+	let txt = `$("#root").empty();$("#layers").empty();`;
 
-				display = clockify(1000 * seconds);
-			}
+	if (noHTMLSupport.includes(hmp.deviceType)) {
+		txt += `$("#root").append('<rect fill="black" height="${height}" id="background" stroke="none" width="${width}"/>');`;
+		txt += `$("#root").append('${svgElement.replace(/\n/g, "")}');`;
+		txt += `(function(){
+				${scriptSVG};
+			})();`;
 
-			$("#timer").text(display);
-		},50);
+		txt += `socket.emit("DOMContentLoaded");`;
+	} else {
+		txt += `(function(){
+			${scriptHTML};
+		})();`;
+	}
 
-		var pos = ${width};
-		setInterval(function(){
-			$("#message").attr("x",pos);
-			pos = pos - 0.0034 * ${width};
-			if(pos < -len * ${fontSizeMessage}){
-				pos = ${width};
-			}
-		},16);
-
-		$("svg").attr("viewBox","0 0 ${width} ${height}");
-		socket.emit("echo",$("svg").attr("viewBox"));
-		`;
-
-export const Generate = () => {
-	//const width = 558; //1920;
-	//const height = 336 //1080;
-	let txt = `$("#root").empty();`;
-	txt += `$("#root").append('<rect fill="black" height="${height}" id="background" stroke="none" width="${width}"/>');`;
-	txt += `$("#root").append('${dastuff.replace(/\n/g, "")}');`;
-	txt += `(function(){
-        ${daScript}
-    })();`;
-	txt += `socket.emit("DOMContentLoaded");`;
 	return txt;
 };
