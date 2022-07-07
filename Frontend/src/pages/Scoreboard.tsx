73bf85ddb6ac3e5531884229652ae641f16cac24
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-07 14:29:21 +0200

    💸 Added sponsored ads to HTML frontend

--- Frontend/src/pages/Scoreboard.tsx
+++ Frontend/src/pages/Scoreboard.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import Clock from "../components/Clock";
 import Flag from "../components/Flag";
 import Scrolltext from "../components/Scrolltext";
@@ -5,9 +7,50 @@ import Appstate from "../utils/Appstate";
 
 export const Scoreboard = () => {
 	const state = Appstate.getState().scoreboard;
-	console.log("#scoreboard", state);
-	return (
-		<div className="p-scoreboard">
+	const [sponsor, setSponsor] = useState("");
+
+	useEffect(() => {
+		let sponsorIndex = 0;
+
+		const interval = setInterval(() => {
+			sponsorIndex++;
+			const state = Appstate.getState().scoreboard;
+			const sponsor = state.sponsors[sponsorIndex % state.sponsors.length];
+			setSponsor(sponsor);
+		}, 5000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, []);
+
+	let layout = "ads";
+	return layout === "ads" ? (
+		<div className="p-scoreboard ads">
+			<div className="scoreboardtop">
+				<header>
+					<div className="team left">
+						<h2 className="teamscore">{state.t1}</h2>
+						<Flag top={state.hb} bottom={state.ho} />
+					</div>
+					<Clock />
+					<div className="team right">
+						<Flag top={state.ub} bottom={state.uo} />
+						<h2 className="teamscore">{state.t2}</h2>
+					</div>
+				</header>
+				<main>
+					<picture>
+						<img src={sponsor} alt="" />
+					</picture>
+				</main>
+			</div>
+			<footer>
+				<Scrolltext text={state.message} />
+			</footer>
+		</div>
+	) : (
+		<div className="p-scoreboard noads">
 			<div className="scoreboardtop">
 				<header>
 					<div className="team">
