174b2ef2b34d8e604a93f998e670540b5bf5bcca
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-11 17:43:36 +0200

    📱 Reworked textedit

--- Frontend/src/components/TextEdit.tsx
+++ Frontend/src/components/TextEdit.tsx
@@ -11,29 +11,26 @@ export const TextEdit = ({ setVisible = () => {} }: { setVisible?: (event?: any)
 	};
 
 	return (
-		<div className="c-textedit__container">
-			<div className="c-textedit__header">
-				<p>Type hier je bericht:</p>
-				<div className="c-textedit__header-btn">
-					<button onClick={() => setVisible(false)}>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round">
-							<line x1="18" y1="6" x2="6" y2="18"></line>
-							<line x1="6" y1="6" x2="18" y2="18"></line>
-						</svg>
-					</button>
-				</div>
-			</div>
-			<div className="c-textedit__textarea">
-				<label htmlFor="scrolltext">Tekst</label>
+		<div className="c-card c-textedit">
+			<header>
+				<button onClick={() => setVisible(false)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round">
+						<line x1="18" y1="6" x2="6" y2="18"></line>
+						<line x1="6" y1="6" x2="18" y2="18"></line>
+					</svg>
+				</button>
+			</header>
+			<div className="textarea-container">
+				<label htmlFor="scrolltext">Scroll tekst</label>
 				<textarea value={message} name="scrolltext" id="scrolltext" onChange={onTextUpdate}></textarea>
 			</div>
 
