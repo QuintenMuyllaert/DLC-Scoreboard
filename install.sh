c589f35f989c029e52e781c5a5fd2d7935e0d17f
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-11 13:49:53 +0200

    👷 Updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-apt update
-apt upgrade
+apt update -y
+apt upgrade -y
+apt autoremove -y
 apt install npm git
-npm i -g pm2 
-pm2 startup
-bash start.sh
+pm2 --version || (npm install -g pm2 && pm2 startup)
+git clone https://github.com/QuintenMuyllaert/DLC-Scoreboard/ 
+cd DLC-Scoreboard/
+git reset --hard
+git pull -f
+git checkout dev
+pm2 delete all
+cd Backend/
+npm i && pm2 start --name="Backend" "npm run dev"
+cd ../Frontend/
+npm i && pm2 start --name="Frontend" "npm run dev"
+pm2 save
