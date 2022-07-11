b1cc2d2cc6648e976b1d55eb2d8b560b30e6bd3d
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-11 18:42:55 +0200

    📱 Usersettings styling rework

--- Frontend/src/pages/UserSettings.tsx
+++ Frontend/src/pages/UserSettings.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import Appstate from "../utils/Appstate";
 
 import BottomTab from "../components/BottomTab";
@@ -6,34 +5,14 @@ import IconButton from "../components/IconButton";
 import Input from "../components/Input";
 
 export const UserSettings = () => {
-	const { color, jwt } = Appstate.getState();
-
-	const refThemeSwitch = useRef<HTMLInputElement>(null);
-
-	const onChangeTheme = () => {
-		console.log(refThemeSwitch.current?.checked);
-
-		if (refThemeSwitch.current?.checked) {
-			localStorage.setItem("theme", "dark");
-			Appstate.updateState("color", "dark");
-		} else {
-			localStorage.setItem("theme", "light");
-			Appstate.updateState("color", "light");
-		}
-	};
+	const { jwt } = Appstate.getState();
 
 	const onClickSave = () => {};
 
-	const onLogout = () => {
-		console.log("logging out");
-		document.location.href = "/logout";
-	};
-
 	return (
 		<>
-			<div className="p-page p-usersettings element">
+			<div className="p-page p-usersettings">
 				<h1>Hallo {jwt?.username}</h1>
-
 				<div className="content">
 					<Input label="Naam" type="text" />
 					<Input label="Huidig wachtwoord" type="password" />
@@ -41,16 +20,7 @@ export const UserSettings = () => {
 					<Input label="Herhaal nieuw wachtwoord" type="password" />
 				</div>
 				<div className="buttons">
-					<div>
-						<p className="title">theme:</p>
-						<label className="switch">
-							<input ref={refThemeSwitch} type="checkbox" defaultChecked={color === "dark"} onChange={onChangeTheme} />
-							<span className="slider"></span>
-						</label>
-					</div>
-					<div className="save">
-						<IconButton label="OPSLAAN" color="white" onClick={onClickSave} />
-					</div>
+					<IconButton label="OPSLAAN" color="white" onClick={onClickSave} />
 				</div>
 			</div>
 			<BottomTab />
