589f35d35ab47062425038826d65353f325453f6
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-12 17:32:48 +0200

    ✏️ Copypasta

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,20 +1,6 @@
-import { useState } from "react";
-import { useRafLoop } from "react-use";
-
-import Appstate from "../utils/Appstate";
-import { calculateClock } from "../utils/Utils";
 import Logo from "./Logo";
 
 export default ({ title, onClick }: { title: string; onClick?: (event?: any) => any }) => {
-	const [value, setValue] = useState("00:00");
-
-	useRafLoop(() => {
-		const time = calculateClock(Appstate.getState().scoreboard.clockData);
-		if (time != value) {
-			setValue(time);
-		}
-	});
-
 	return (
 		<nav className="c-header">
 			<div></div>
