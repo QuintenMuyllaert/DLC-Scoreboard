ae6aa305fc79197da24a470322e7a9a6f1aa551c
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-14 12:11:49 +0200

    📱 Manual responsiveness

--- Frontend/src/pages/Manual.tsx
+++ Frontend/src/pages/Manual.tsx
@@ -130,70 +130,61 @@ export default () => {
 	return (
 		<div className="p-page p-manual">
 			<div className="content">
-				<div className="u-grid-vertical-gap">
-					<p className={validationState.display ? "validatie" : "hidden"}>{validationState.message}</p>
-					<Input
-						id="serienummer"
-						label="serienummer"
-						type="text"
-						disabled={!state.hasScoreboard}
-						onChange={(event: React.FormEvent<HTMLInputElement>) => {
-							updateState("serial", event.currentTarget.value);
-						}}
-					/>
-					<Input
-						id="username"
-						label="username"
-						type="text"
-						onChange={(event: React.FormEvent<HTMLInputElement>) => {
-							updateState("username", event.currentTarget.value);
-						}}
-					/>
-					<Input
-						id="password"
-						label="wachtwoord"
-						type="password"
-						onChange={(event: React.FormEvent<HTMLInputElement>) => {
-							updateState("password", event.currentTarget.value);
-						}}
-					/>
-					<Input
-						id="confirmpassword"
-						label="bevestig wachtwoord"
-						type="password"
-						onChange={(event: React.FormEvent<HTMLInputElement>) => {
-							updateState("confirmPassword", event.currentTarget.value);
-						}}
-					/>
-					<div className="c-checkbox">
-						<input type="checkbox" id="noScoreboaard" name="noScoreboaard" value="no" onClick={onCheck} />
-						<label htmlFor="saveTemplate">Ik heb geen scorebord</label>
-					</div>
-				</div>
-				<div className="button">
-					<IconButton
-						icon={
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								strokeWidth="2"
-								strokeLinecap="round"
-								strokeLinejoin="round">
-								<path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
-								<polyline points="10 17 15 12 10 7"></polyline>
-								<line x1="15" y1="12" x2="3" y2="12"></line>
-							</svg>
-						}
-						label="REGISTREREN"
-						color="white"
-						onClick={sendRegisterRequest}
-					/>
-				</div>
+				<Input
+					id="serienummer"
+					label="serienummer"
+					type="text"
+					disabled={!state.hasScoreboard}
+					onChange={(event: React.FormEvent<HTMLInputElement>) => {
+						updateState("serial", event.currentTarget.value);
+					}}
+				/>
+				<Input
+					id="username"
+					label="username"
+					type="text"
+					onChange={(event: React.FormEvent<HTMLInputElement>) => {
+						updateState("username", event.currentTarget.value);
+					}}
+				/>
+				<Input
+					id="password"
+					label="wachtwoord"
+					type="password"
+					onChange={(event: React.FormEvent<HTMLInputElement>) => {
+						updateState("password", event.currentTarget.value);
+					}}
+				/>
+				<Input
+					id="confirmpassword"
+					label="bevestig wachtwoord"
+					type="password"
+					onChange={(event: React.FormEvent<HTMLInputElement>) => {
+						updateState("confirmPassword", event.currentTarget.value);
+					}}
+				/>
 			</div>
+			<IconButton
+				icon={
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round">
+						<path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+						<polyline points="10 17 15 12 10 7"></polyline>
+						<line x1="15" y1="12" x2="3" y2="12"></line>
+					</svg>
+				}
+				label="REGISTREREN"
+				color="white"
+				onClick={sendRegisterRequest}
+			/>
 		</div>
 	);
 };
