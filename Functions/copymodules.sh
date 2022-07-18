503b41a57cc91a09d173d76f0e53a5d0c89dc247
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-18 18:22:25 +0200

    👷 Copymodules update

--- Functions/copymodules.sh
+++ Functions/copymodules.sh
@@ -1,3 +1,7 @@
 cp -R -T modules/ ./packages/dlcscoreboard/auth/modules/
 cp -R -T modules/ ./packages/dlcscoreboard/status/modules/
 cp -R -T modules/ ./packages/dlcscoreboard/readtest/modules/
+cp -R -T modules/ ./packages/dlcscoreboard/register/modules/
+cp -R -T modules/ ./packages/dlcscoreboard/revoke/modules/
+cp -R -T modules/ ./packages/dlcscoreboard/logout/modules/
+cp -R -T modules/ ./packages/dlcscoreboard/changepassword/modules/
