b36b4d3afff51512a9b1dea43ebc17ef6684e6e5
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-18 18:20:11 +0200

    ⚗️ Change password

---
+++ Functions/packages/dlcscoreboard/changepassword/test/index.test.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
+const { main } = require("../index");
+
+/*describe("Test changepassword", () => {
+  test("Changepassword", async () => {
+    const response = await main();
+    expect(response.body).toEqual(false);
+  });
+});*/
