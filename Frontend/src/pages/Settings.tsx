dced2a73c417da9b571214ef54b3cc7f132b1d85
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-25 17:02:10 +0200

    ✨ Added switchscoreboard route to settings

--- Frontend/src/pages/Settings.tsx
+++ Frontend/src/pages/Settings.tsx
@@ -149,6 +149,29 @@ export default () => {
 							</svg>
 						}
 					/>
+					<IconButton
+						color="white"
+						label="Scoreboards"
+						onClick={() => navigate("/switchscoreboard")}
+						icon={
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round">
+								<rect x="9" y="2" width="6" height="6"></rect>
+								<rect x="16" y="16" width="6" height="6"></rect>
+								<rect x="2" y="16" width="6" height="6"></rect>
+								<path d="M5 16v-4h14v4"></path>
+								<path d="M12 12V8"></path>
+							</svg>
+						}
+					/>
 				</main>
 			</div>
 
