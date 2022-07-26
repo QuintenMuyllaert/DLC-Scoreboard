1d326b909a3a7642ef4e982779eccf3563fc9281
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-07-26 21:18:24 +0200

    💄 UI fixes

--- Frontend/src/pages/Schedule.tsx
+++ Frontend/src/pages/Schedule.tsx
@@ -92,7 +92,7 @@ export default () => {
 
 	return (
 		<>
-			<Header title="Settings" icon={<Backarrow />} />
+			<Header title="Schedule" icon={<Backarrow />} />
 			<div className="p-page p-schedule">
 				<main>
 					<div className="timecontainer">
@@ -127,10 +127,12 @@ export default () => {
 					<div className="c-option">
 						<label htmlFor="selectedTemplate">Reel selecteren</label>
 						<select id="selectedTemplate" onChange={onChangeTemplate}>
-							<option value="0" selected>
-								Selecteer een template
-							</option>
-							{options}
+							<>
+								<option value="0" selected>
+									Selecteer een template
+								</option>
+								{options}
+							</>
 						</select>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -146,29 +148,7 @@ export default () => {
 						</svg>
 					</div>
 
-					<IconButton
-						color="white"
-						label="Opslaan"
-						onClick={onClickSave}
-						icon={
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								strokeWidth="2"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								className="c-bottomtab__page-icon">
-								<path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-								<circle cx="9" cy="7" r="4"></circle>
-								<path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-								<path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-							</svg>
-						}
-					/>
+					<IconButton color="white" label="Opslaan" onClick={onClickSave} />
 				</main>
 			</div>
 
--- Frontend/src/pages/Switchscoreboard.tsx
+++ Frontend/src/pages/Switchscoreboard.tsx
@@ -49,7 +49,7 @@ export default () => {
 
 	return (
 		<>
-			<Header title="Settings" icon={<Backarrow />} />
+			<Header title="Switch Scoreboard" icon={<Backarrow />} />
 			<div className="p-page p-schedule">
 				<main>
 					<div className="c-option">
@@ -76,29 +76,7 @@ export default () => {
 						</svg>
 					</div>
 
-					<IconButton
-						color="white"
-						label="Opslaan"
-						onClick={onClickSave}
-						icon={
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								strokeWidth="2"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								className="c-bottomtab__page-icon">
-								<path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-								<circle cx="9" cy="7" r="4"></circle>
-								<path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-								<path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-							</svg>
-						}
-					/>
+					<IconButton color="white" label="Opslaan" onClick={onClickSave} />
 				</main>
 			</div>
 
